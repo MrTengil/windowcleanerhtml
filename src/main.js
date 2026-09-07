dfb8db4347d747a56e132e76eeec0d6c9832c933
Oskar Tengberg <oskar.tengberg@hemnet.se>
2026-09-07 15:40:20 +0200

    feat: size the game canvas to the viewport's aspect ratio

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import { BootScene } from "./scenes/BootScene.js";
 import { MainMenuScene } from "./scenes/MainMenuScene.js";
 import { HouseCleanScene } from "./scenes/HouseCleanScene.js";
+import { computeGameWidth } from "./utils/viewport.js";
+
+const DESIGN_HEIGHT = 1560;
+const MIN_ASPECT = 0.4615;
+const MAX_ASPECT = 0.65;
+
+const width = computeGameWidth({
+  viewportWidth: window.innerWidth,
+  viewportHeight: window.innerHeight,
+  designHeight: DESIGN_HEIGHT,
+  minAspect: MIN_ASPECT,
+  maxAspect: MAX_ASPECT,
+});
 
 new Phaser.Game({
   type: Phaser.AUTO,
   parent: "game",
-  width: 720,
-  height: 1560,
+  width,
+  height: DESIGN_HEIGHT,
   backgroundColor: "#14161c",
   scale: {
     mode: Phaser.Scale.FIT,
