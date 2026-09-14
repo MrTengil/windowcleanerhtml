f8f6bc6a64b5adb7a356094a92363be4c6ff4b68
Oskar Tengberg <oskar.tengberg@hemnet.se>
2026-09-14 16:31:07 +0200

    feat: make the sticker three times more stubborn to remove

--- src/config/dirtTypes.js
+++ src/config/dirtTypes.js
@@ -31,7 +31,7 @@ export const DIRT_TYPES = {
     interactionType: "wipe",
     toolIds: ["squeegee"],
     requiredSprayStage: 0,
-    hitsToClean: 48,
+    hitsToClean: 144,
     color: 0xdba844,
     stages: [
       { textureKey: "sticker-stage-1", file: "stickers/sticker_peel_stage_1.svg" },
